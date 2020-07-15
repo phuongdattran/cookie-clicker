34c06ec38b969aa64af8f3d648c28866d62defad
phuongdattran <tran.phuongdat@gmail.com>
2020-07-15 14:10:42 +0200

    Version 1

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-let click =  document.getElementById("click") ;        
-let label =  document.getElementById("label") ;     
-let labelContent = label.innerHTML; 
-let multi =  document.getElementById("multi") ;    
-let multiOn = false ;
+let click = document.getElementById("click");
+let label = document.getElementById("label");
+let labelContent = label.innerHTML;
+let multi = document.getElementById("multi");
+let multiOn = false;
 let multiplier = 2;
-let counterMulti = 0;
-let cost = 1;
-cost = cost*counterMulti;
-document.getElementById("cost").innerHTML = multiplier*counterMulti;
+let counterMulti = 1;
+document.getElementById("cost").innerHTML =
+  "cost: " + multiplier * counterMulti;
+let auto = document.getElementById("auto");
+let autoOn = false;
 
 click.addEventListener("click", increment);
-multi.addEventListener("click", ()=>{
+multi.addEventListener("click", () => {
+  if (parseInt(label.innerHTML) >= multiplier * counterMulti) {
+    multiOn = true;
+    label.innerHTML = parseInt(label.innerHTML) - multiplier * counterMulti;
+    counterMulti++;
+    document.getElementById("cost").innerHTML =
+      "cost: " + multiplier * counterMulti;
+    multi.innerHTML = "Multiplier: X" + multiplier * (counterMulti - 1);
+  }
+});
 
-    if (parseInt(label.innerHTML)>=multiplier*counterMulti){
-        document.getElementById("cost").innerHTML = multiplier*(counterMulti+1);
-        multiOn=true;
-        counterMulti++;
-        label.innerHTML = parseInt(label.innerHTML)-multiplier*counterMulti;
-        multi.innerHTML = "Multiplier: X" + multiplier*counterMulti;
-    }
-})
+function increment() {
+  if (multiOn == false) {
+    label.innerHTML = parseInt(label.innerHTML) + 1;
+  } else {
+    label.innerHTML =
+      parseInt(label.innerHTML) + multiplier * (counterMulti - 1);
+  }
+}
 
-
-
-
-
-function increment ( )  { 
-    if (multiOn == false)
-        {label.innerHTML= parseInt(label.innerHTML)+1;}
-    else {label.innerHTML = parseInt(label.innerHTML)+multiplier*counterMulti;};
-} 
-
-
-
-/*let 
-let susbract = labelContent-cost; */
-
-
-
-/* susbract = on enlève le cout au label 
-je dois définir le cout dans une variable */
+auto.addEventListener("click", autoIncrement);
+function autoIncrement() {
+  let intervalAuto;
+  if (autoOn == false) {
+    autoOn == true;
+    intervalAuto = setInterval(increment, 1000);
+  } else {
+    autoOn = false;
+    clearInterval(intervalAuto);
+  }
+}
