be32a8855736b80678c807440e1c06746fffd907
phuongdattran <tran.phuongdat@gmail.com>
2020-07-15 16:25:40 +0200

    Add bonus feature

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ let multiOn = false;
 let multiplier = 2;
 let counterMulti = 1;
 document.getElementById("cost").innerHTML =
-  "cost: " + multiplier * counterMulti;
+  "Next multiplier cost: " + multiplier * counterMulti;
 let auto = document.getElementById("auto");
 let autoOn = false;
+let bonus = document.getElementById("bonus");
 
 click.addEventListener("click", increment);
 multi.addEventListener("click", () => {
@@ -17,7 +18,7 @@ multi.addEventListener("click", () => {
     label.innerHTML = parseInt(label.innerHTML) - multiplier * counterMulti;
     counterMulti++;
     document.getElementById("cost").innerHTML =
-      "cost: " + multiplier * counterMulti;
+      "Next multiplier cost: " + multiplier * counterMulti;
     multi.innerHTML = "Multiplier: X" + multiplier * (counterMulti - 1);
   }
 });
@@ -31,14 +32,45 @@ function increment() {
   }
 }
 
+function increment2() {
+  if (multiOn == false) {
+    label.innerHTML = parseInt(label.innerHTML) + 1;
+  } else {
+    label.innerHTML =
+      parseInt(label.innerHTML) + multiplier * (counterMulti - 1);
+  }
+}
+
 auto.addEventListener("click", autoIncrement);
 function autoIncrement() {
-  let intervalAuto;
-  if (autoOn == false) {
-    autoOn == true;
-    intervalAuto = setInterval(increment, 1000);
-  } else {
-    autoOn = false;
-    clearInterval(intervalAuto);
+  let autoCost = 200;
+  if (parseInt(label.innerHTML) >= autoCost && autoOn == false) {
+    autoOn = true;
+    label.innerHTML = parseInt(label.innerHTML) - autoCost;
+    setInterval(increment, 1000);
   }
+}
+
+bonus.addEventListener("click", boost);
+function boost() {
+  let boostCost = 100;
+  if (parseInt(label.innerHTML) >= boostCost) {
+    label.innerHTML = parseInt(label.innerHTML) - boostCost;
+    click.addEventListener("click", increment2);
+    time(30);
+    setTimeout(() => {
+      click.removeEventListener("click", increment2);
+    }, 30000);
+  }
+}
+
+function time(currentTime) {
+  setInterval(() => {
+    if (currentTime > 0) {
+      currentTime = currentTime - 1;
+      bonus.innerHTML = "Time left : " + currentTime;
+    } else {
+      bonus.innerHTML = "Bonus";
+    }
+  }, 1000);
 }
